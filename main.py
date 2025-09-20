6ed61884b53ea2e4c394f0abd63b55b73e567717
Veneomen <veniaminmolcan@gmail.com>
2025-09-20 17:47:15 +0300

    with decoration

--- main.py
+++ main.py
@@ -2,24 +2,31 @@ import sys
 import os
 import classes
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QFont
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Info about your computer")
         self.setGeometry(700, 300, 500, 500)
+        self.setStyleSheet("background-color: black")
         self.UI()
     def UI(self):
         self.MainLayout=QVBoxLayout()
         self.button=QPushButton("Info", self)
+        self.button.setStyleSheet("color: green;")
         self.button.clicked.connect(self.clicked1)
         self.button2=QPushButton("System", self)
+        self.button2.setStyleSheet("color: green;")
         self.button2.clicked.connect(self.clicked2)
         self.button3=QPushButton("Memory", self)
+        self.button3.setStyleSheet("color: green")
         self.button3.clicked.connect(self.clicked3)
         self.button4=QPushButton("Path", self)
+        self.button4.setStyleSheet("color: green;")
         self.button4.clicked.connect(self.clicked4)
         self.label=QLabel("")
+        self.label.setStyleSheet("color: green;")
         self.MainLayout.addWidget(self.label)
         self.MainLayout.addWidget(self.button)
         self.MainLayout.addWidget(self.button2)
